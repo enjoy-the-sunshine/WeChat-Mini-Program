1360a7824d84cfcb75af8b3c789ea2f8e9cead09
Yolandamujiu <13819274080@163.com>
2025-08-30 14:28:21 +0800

    8.30
    除leancloud配置外已基本实现功能和布局

--- person/pages/account-security/account-security.js
+++ person/pages/account-security/account-security.js
@@ -1,8 +1,74 @@
 Page({
   data: {
-    // 页面级数据在这里定义
+    modalVisible: false,
+    modalTitle: '',
+    modalInput: '',
+    currentItem: '',
+    oldPassword: 'yourOldPassword', // Set this to the actual old password
+    newPassword: '',
+    // Other data properties...
   },
-  onLoad: function() {
-    // 页面加载时的逻辑处理
+  
+  showModal(event) {
+    const item = event.currentTarget.dataset.item;
+    let title = '';
+    
+    switch (item) {
+      case '旧密码':
+        title = '请输入旧密码';
+        this.setData({ currentItem: '旧密码' });
+        break;
+      case '新密码':
+        title = '请输入新密码';
+        this.setData({ currentItem: '新密码' });
+        break;
+      // Other cases...
+    }
+    
+    this.setData({
+      modalVisible: true,
+      modalTitle: title,
+      modalInput: ''
+    });
+  },
+  
+  hideModal() {
+    this.setData({
+      modalVisible: false
+    });
+  },
+  
+  updateInput(e) {
+    this.setData({
+      modalInput: e.detail.value
+    });
+  },
+  
+  confirmModal() {
+    const { currentItem, modalInput, oldPassword } = this.data;
+    
+    if (currentItem === '旧密码') {
+      if (modalInput === oldPassword) {
+        this.setData({
+          modalTitle: '请输入新密码',
+          currentItem: '新密码',
+          modalInput: ''
+        });
+      } else {
+        wx.showToast({
+          title: '旧密码错误',
+          icon: 'none'
+        });
+      }
+    } else if (currentItem === '新密码') {
+      this.setData({
+        newPassword: modalInput
+      });
+      wx.showToast({
+        title: '密码修改成功',
+        icon: 'success'
+      });
+      this.hideModal();
+    }
   }
 });
