e5033196cccb145d7c434c62f3ee971ab22da0db
dlk0037 <172147727@qq.com>
2025-08-31 15:11:32 +0800

    Add files via upload
    来自咖啡字典

---
+++ miniprogram-14/pages/bawang/bawang.js
@@ -0,0 +1,302 @@
+Page({
+  data: {
+    coffeeData: 
+    [
+ { id: 103, name: "伯牙绝弦", isSelected: false, modal: { title: "伯牙绝弦", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "104mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "128mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "137mg", isActive: false } ] } }, { id: 104, name: "山野栀子", isSelected: false, modal: { title: "山野栀子", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "130mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "161mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "172mg", isActive: false } ] } }, { id: 105, name: "醒时春山", isSelected: false, modal: { title: "醒时春山", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "126mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "155mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "166mg", isActive: false } ] } }, { id: 106, name: "青青糯山", isSelected: false, modal: { title: "青青糯山", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "68mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "84mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "89mg", isActive: false } ] } }, { id: 107, name: "桂馥兰香", isSelected: false, modal: { title: "桂馥兰香", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "106mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "131mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "140mg", isActive: false } ] } }, { id: 108, name: "白雾红尘", isSelected: false, modal: { title: "白雾红尘", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "84mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "103mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "110mg", isActive: false } ] } }, { id: 109, name: "去云南·玫瑰普洱", isSelected: false, modal: { title: "去云南·玫瑰普洱", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "165mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "204mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "218mg", isActive: false } ] } }, { id: 110, name: "花田乌龙", isSelected: false, modal: { title: "花田乌龙", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "73mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "90mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "97mg", isActive: false } ] } }, { id: 111, name: "万山红·金丝小种", isSelected: false, modal: { title: "万山红·金丝小种", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "123mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "151mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "162mg", isActive: false } ] } }, { id: 112, name: "青沫观音", isSelected: false, modal: { title: "青沫观音", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "125mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "154mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "164mg", isActive: false } ] } }, { id: 113, name: "桂子飘飘", isSelected: false, modal: { title: "桂子飘飘", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "84mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "104mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "111mg", isActive: false } ] } }, { id: 114, name: "关山木兰", isSelected: false, modal: { title: "关山木兰", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "125mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "155mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "165mg", isActive: false } ] } }, { id: 115, name: "春日韬韬", isSelected: false, modal: { title: "春日韬韬", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "66mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "81mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "87mg", isActive: false } ] } }, { id: 116, name: "千山雪·金丝小种", isSelected: false, modal: { title: "千山雪·金丝小种", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "76mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "94mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "100mg", isActive: false } ] } }, { id: 117, name: "杨枝甘露", isSelected: false, modal: { title: "杨枝甘露", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "28mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "35mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "37mg", isActive: false } ] } }, { id: 118, name: "云海芒芒", isSelected: false, modal: { title: "云海芒芒", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "50mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "61mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "66mg", isActive: false } ] } }, { id: 119, name: "神仙椰水", isSelected: false, modal: { title: "神仙椰水", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "0mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "0mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "0mg", isActive: false } ] } }, { id: 120, name: "观音仙椰", isSelected: false, modal: { title: "观音仙椰", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "16mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "19mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "20mg", isActive: false } ] } }, { id: 121, name: "茉莉仙椰", isSelected: false, modal: { title: "茉莉仙椰", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "30mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "37mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "40mg", isActive: false } ] } }, { id: 122, name: "海盐电解·柠檬茶", isSelected: false, modal: { title: "海盐电解·柠檬茶", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "58mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "72mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "77mg", isActive: false } ] } }, { id: 123, name: "清凉因子·柠檬茶", isSelected: false, modal: { title: "清凉因子·柠檬茶", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "70mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "86mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "92mg", isActive: false } ] } }, { id: 124, name: "浮生梦媞", isSelected: false, modal: { title: "浮生梦媞", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "47mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "59mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "63mg", isActive: false } ] } }, { id: 125, name: "葡萄碎玉", isSelected: false, modal: { title: "葡萄碎玉", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "42mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "52mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "55mg", isActive: false } ] } }, { id: 126, name: "千峰翠", isSelected: false, modal: { title: "千峰翠", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "71mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "87mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "93mg", isActive: false } ] } }, { id: 127, name: "琥珀光", isSelected: false, modal: { title: "琥珀光", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "56mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "70mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "74mg", isActive: false } ] } }, { id: 128, name: "橙香四季", isSelected: false, modal: { title: "橙香四季", selectedIndex: -1, items: [ { type: "中杯", volume: "470ml", caffeine: "67mg", isActive: false }, { type: "纸杯大杯", volume: "580ml", caffeine: "83mg", isActive: false }, { type: "塑料杯大杯", volume: "620ml", caffeine: "89mg", isActive: false } ] } },{
+id: 129,
+name: "七里香",
+isSelected: false,
+modal: {
+title: "七里香",
+selectedIndex: -1,
+items: [
+{type: "中杯", volume: "470ml", caffeine: "57mg", isActive: false},
+{ type: "纸杯大杯", volume: "580ml", caffeine: "70mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "75mg", isActive: false }
+]
+}
+},
+{
+id: 130,
+name: "折桂令",
+isSelected: false,
+modal: {
+title: "折桂令",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "138mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "170mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "182mg", isActive: false }
+]
+}
+},
+{
+id: 131,
+name: "酌红袍",
+isSelected: false,
+modal: {
+title: "酌红袍",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "79mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "97mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "104mg", isActive: false }
+]
+}
+},
+{
+id: 132,
+name: "云中绿",
+isSelected: false,
+modal: {
+title: "云中绿",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "98mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "121mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "129mg", isActive: false }
+]
+}
+},
+{
+id: 133,
+name: "花田坞",
+isSelected: false,
+modal: {
+title: "花田坞",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "94mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "115mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "123mg", isActive: false }
+]
+}
+},
+{
+id: 134,
+name: "野栀子",
+isSelected: false,
+modal: {
+title: "野栀子",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "141mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "173mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "185mg", isActive: false }
+]
+}
+},
+{
+id: 135,
+name: "木兰辞",
+isSelected: false,
+modal: {
+title: "木兰辞",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "130mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "160mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "171mg", isActive: false }
+]
+}
+},
+{
+id: 136,
+name: "观音韵",
+isSelected: false,
+modal: {
+title: "观音韵",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "123mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "151mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "162mg", isActive: false }
+]
+}
+},
+{
+id: 137,
+name: "陈柑普洱茶拿铁",
+isSelected: false,
+modal: {
+title: "陈柑普洱茶拿铁",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "180mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "222mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "237mg", isActive: false }
+]
+}
+},
+{
+id: 138,
+name: "大红袍茶拿铁",
+isSelected: false,
+modal: {
+title: "大红袍茶拿铁",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "131mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "161mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "172mg", isActive: false }
+]
+}
+},
+{
+id: 139,
+name: "正山小种茶拿铁",
+isSelected: false,
+modal: {
+title: "正山小种茶拿铁",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "125mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "154mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "165mg", isActive: false }
+]
+}
+},
+{
+id: 140,
+name: "七窨・茉莉雪芽",
+isSelected: false,
+modal: {
+title: "七窨・茉莉雪芽",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "65mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "80mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "86mg", isActive: false }
+]
+}
+},
+{
+id: 141,
+name: "慢焙・金观音",
+isSelected: false,
+modal: {
+title: "慢焙・金观音",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "64mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "79mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "85mg", isActive: false }
+]
+}
+},
+{
+id: 142,
+name: "轻酵・金桂乌龙",
+isSelected: false,
+modal: {
+title: "轻酵・金桂乌龙",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "31mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "38mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "41mg", isActive: false }
+]
+}
+},
+{
+id: 143,
+name: "生晒・陈柑普洱",
+isSelected: false,
+modal: {
+title: "生晒・陈柑普洱",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "106mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "131mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "140mg", isActive: false }
+]
+}
+},
+{
+id: 144,
+name: "混捻・糯香绿茶",
+isSelected: false,
+modal: {
+title: "混捻・糯香绿茶",
+selectedIndex: -1,
+items: [
+{ type: "中杯", volume: "470ml", caffeine: "45mg", isActive: false },
+{ type: "纸杯大杯", volume: "580ml", caffeine: "56mg", isActive: false },
+{ type: "塑料杯大杯", volume: "620ml", caffeine: "60mg", isActive: false }
+]
+}
+}
+    ],
+          
+    showModal: false,
+    currentModal: {}
+  },
+
+  handleItemTap(e) {
+    const index = e.currentTarget.dataset.index;
+    const selectedCoffee = this.data.coffeeData[index];
+
+    // 重置所有咖啡的选中状态
+    const updatedList = this.data.coffeeData.map((item, i) => {
+      item.isSelected = i === index;
+      return item;
+    });
+
+    this.setData({
+      coffeeData: updatedList,
+      currentModal: selectedCoffee.modal,
+      showModal: true
+    });
+  },
+
+  selectCupType(e) {
+    const index = e.currentTarget.dataset.index;
+    const title = this.data.currentModal.title;
+    
+    // 找到当前操作的咖啡项
+    const coffeeIndex = this.data.coffeeData.findIndex(item => item.modal.title === title);
+    if (coffeeIndex !== -1) {
+      // 更新当前咖啡项的杯型选中状态
+      const updatedCoffeeData = [...this.data.coffeeData];
+      updatedCoffeeData[coffeeIndex].modal.items.forEach((item, i) => {
+        item.isActive = i === index;
+      });
+      
+      // 更新选中索引
+      updatedCoffeeData[coffeeIndex].modal.selectedIndex = index;
+      
+      // 更新数据
+      this.setData({
+        coffeeData: updatedCoffeeData,
+        currentModal: updatedCoffeeData[coffeeIndex].modal
+      });
+    }
+  },
+
+  closeModal() {
+    this.setData({
+      showModal: false
+    });
+  },
+
+  confirmSelection() {
+    const { title, selectedIndex, items } = this.data.currentModal;
+    if (selectedIndex === -1) {
+      wx.showToast({
+        title: "请选择一个杯型",
+        icon: "none"
+      });
+      return;
+    }
+    const selectedCup = items[selectedIndex];
+    wx.showToast({
+      title: `已选择${title}的${selectedCup.type}杯型`,
+      icon: "none"
+    });
+    this.closeModal();
+  },
+
+  toHome() { /* 跳转首页逻辑 */ },
+  toCoffeeRecord() { /* 跳转咖啡记录逻辑 */ },
+  toMy() { /* 跳转我的逻辑 */ }
+});
---
+++ miniprogram-14/pages/index/index.js
@@ -0,0 +1,72 @@
+Page({
+  data: {
+    starbucksBadge: true, // 控制星巴克徽章显示，根据需求动态修改
+  },
+  // 自定义饮品跳转（需补充实际页面路径）
+  toCustomDrink() {
+    wx.navigateTo({
+      url: '/pages/choose/choose',
+    });
+  },
+  // 星巴克跳转（需补充实际页面路径）
+  toStarbucks() {
+    wx.navigateTo({
+      url: '/pages/xingbake/xingbake',
+    });
+  },
+  // 瑞幸跳转（需补充实际页面路径）
+  toLuckin() {
+    wx.navigateTo({
+      url: '/pages/ruixing/ruixing',
+    });
+  },
+  // 库迪跳转（需补充实际页面路径）
+  toKudi() {
+    wx.navigateTo({
+      url: '/pages/kudi/kudi',
+    });
+  },
+  // 霸王茶姬跳转（需补充实际页面路径）
+  toBawangChaji() {
+    wx.navigateTo({
+      url: '/pages/bawang/bawang',
+    });
+  },
+  // 茶百道跳转（需补充实际页面路径）
+  toChabaidao() {
+    wx.navigateTo({
+      url: '/pages/chabaidao/chabaidao',
+    });
+  },
+  // 古茗跳转（需补充实际页面路径）
+  toGuming() {
+    wx.navigateTo({
+      url: '/pages/guming/guming',
+    });
+  },
+  // 喜茶跳转（需补充实际页面路径）
+  toXicha() {
+    wx.navigateTo({
+      url: '/pages/xicha/xicha',
+    });
+  },
+  // 底部导航跳转
+  toHome() {
+    wx.switchTab({
+      url: '/pages/home/home',
+    });
+  },
+  toCoffeeRecord() {
+    wx.switchTab({
+      url: '/pages/coffeeRecord/coffeeRecord',
+    });
+  },
+  toMy() {
+    wx.switchTab({
+      url: '/pages/my/my',
+    });
+  },
+  onLoad() {
+    // 页面加载逻辑可扩展
+  },
+});
